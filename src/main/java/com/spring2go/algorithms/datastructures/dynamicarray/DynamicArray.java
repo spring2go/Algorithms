1c59e01ff15f1949e5093c3ad77247086bf97e57
bobo <bobo@spring2go.com>
2020-06-21 15:39:57 +0800

    add source code for dynamic array

---
+++ src/main/java/com/spring2go/algorithms/datastructures/dynamicarray/DynamicArray.java
@@ -0,0 +1,81 @@
+package com.spring2go.algorithms.datastructures.dynamicarray;
+
+import java.util.Iterator;
+
+/**
+ * 动态数组实现。
+ *
+ * 用你的实现代码替换YOUR CODE HERE
+ *
+ * Created on Jun, 2020 by @author bobo
+ */
+public class DynamicArray<T> implements Iterable<T> {
+
+    public DynamicArray() {
+        // YOUR CODE HERE
+    }
+
+    public DynamicArray(int capacity) {
+        // YOUR CODE HERE
+    }
+
+    public int size() {
+        // YOUR CODE HERE
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        // YOUR CODE HERE
+        return false;
+    }
+
+    public T get(int index) {
+        // YOUR CODE HERE
+        return null;
+    }
+
+    public void set(int index, T elem) {
+        // YOUR CODE HERE
+    }
+
+    public void clear() {
+        // YOUR CODE HERE
+    }
+
+    public void add(T elem) {
+        // YOUR CODE HERE
+    }
+
+    public T removeAt(int index) {
+        // YOUR CODE HERE
+        return null;
+    }
+
+    public boolean remove(Object obj) {
+        // YOUR CODE HERE
+        return true;
+    }
+
+    public int indexOf(Object obj) {
+        // YOUR CODE HERE
+        return 0;
+    }
+
+    public boolean contains(Object obj) {
+        // YOUR CODe HERE
+        return false;
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        // YOUR CODE HERE
+        return "";
+    }
+
+}
